b1ff6aed246422c9b9c334cc8a260e33655d1542
hira22 <masayuki.hiraoka@ietty.co.jp>
2021-03-19 01:46:14 +0900

    sum を Int に変更

--- Part1_5NumbersSum/Part1_5NumbersSum/ContentView.swift
+++ Part1_5NumbersSum/Part1_5NumbersSum/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var values: [String] = ["", "", "", "", ""]    
-    @State private var sum: String = "Label"
+    @State private var sum: Int = 0
     
     var body: some View {
         GeometryReader { geometry in
             
             VStack(spacing: 16) {
                 
-                ForEach(0..<values.count) { index in
+                ForEach(0 ..< values.count) { index in
                     TextField("", text: $values[index])
                 }
                 .keyboardType(.numberPad)
@@ -24,23 +24,19 @@ struct ContentView: View {
                 .frame(width: 100)
                 
                 Button("Button") {
-                    let sum = values.reduce(0) { (result, value) in
+                    sum = values.reduce(0) { (result, value) in
                         return result + (Int(value) ?? 0)
                     }
-                    
-                    self.sum = String(sum)
                 }
                 
-                Text(sum)
+                Text(sum > 0 ? String(sum) : "Label")
                     .frame(width: 100, alignment: .leading)
             }
+            .padding()
             .frame(width: geometry.frame(in: .global).width,
                    height: geometry.frame(in: .global).height,
                    alignment: .topLeading)
-            
         }
-        .padding()
-
     }
 }
 
