e233d2b05cb2a3e83c9df135654e635814d74f9b
hira22 <masayuki.hiraoka@ietty.co.jp>
2021-03-20 19:37:25 +0900

    Part2 initial commit

---
+++ Part2_BasicArithmeticOperations/Part2_BasicArithmeticOperations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Part2_BasicArithmeticOperations
+//
+//  Created by hiraoka on 2021/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Part2_BasicArithmeticOperations/README.md
@@ -0,0 +1,3 @@
+#  Part2 足し算・引き算・掛け算・割り算するアプリを作る
+
+https://youtu.be/gxpeP77DfPA
